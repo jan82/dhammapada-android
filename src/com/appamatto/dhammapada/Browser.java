adb3913fb15052cf6594ee2595f1f3afa020f85e
Matthew Willis <appamatto@gmail.com>
2011-02-13 00:44:09 -0600

    Remember your place

--- src/com/appamatto/dhammapada/Browser.java
+++ src/com/appamatto/dhammapada/Browser.java
@@ -1,6 +1,7 @@
 package com.appamatto.dhammapada;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -8,6 +9,7 @@ import android.widget.ListView;
 public class Browser extends Activity {
 	private SQLiteDatabase db;
 	private VersesAdapter adapter;
+	private ListView verses;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -16,13 +18,20 @@ public class Browser extends Activity {
 
 		db = new DBHelper(this).getReadableDatabase();
 		adapter = new VersesAdapter(this, db);
-		ListView verses = (ListView) findViewById(R.id.verses_list);
+		verses = (ListView) findViewById(R.id.verses_list);
 		verses.setDividerHeight(5);
 		verses.setAdapter(adapter);
+
+		SharedPreferences prefs = getSharedPreferences("Browser", MODE_PRIVATE);
+		int position = prefs.getInt("progress", 0);
+		verses.setSelection(position);
 	}
 
 	@Override
 	public void onDestroy() {
+		int position = verses.getFirstVisiblePosition();
+		SharedPreferences prefs = getSharedPreferences("Browser", MODE_PRIVATE);
+		prefs.edit().putInt("progress", position).commit();
 		super.onDestroy();
 		adapter.close();
 		adapter = null;
